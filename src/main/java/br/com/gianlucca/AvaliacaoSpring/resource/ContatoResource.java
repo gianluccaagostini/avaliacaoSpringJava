fc6b391cb5b89d83c0576342e01df90bb3bd0070
Gianlucca Agostini <gianluccaagostini97@gmail.com>
2023-11-02 00:54:05 -0300

    Ajustes na classe Resource contato

--- src/main/java/br/com/gianlucca/AvaliacaoSpring/resource/ContatoResource.java
+++ src/main/java/br/com/gianlucca/AvaliacaoSpring/resource/ContatoResource.java
@@ -1,6 +1,6 @@
 package br.com.gianlucca.AvaliacaoSpring.resource;
 
-import java.util.List;
+
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,7 +27,7 @@ public class ContatoResource {
 	public ContatoResource(ContatoService contatoService) {
 		this.contatoService = contatoService;
 	}
-	/*
+	
 	@GetMapping("/{id}")
 	public ResponseEntity<Optional<Contato>> getById(@PathVariable Long id){
 		Optional<Contato> contato = contatoService.getById(id);
@@ -34,15 +35,18 @@ public class ContatoResource {
 			return ResponseEntity.notFound().build();
 		}
 		return ResponseEntity.ok(contato);
-	}*/
+	}
+		
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Contato> update(@RequestBody Contato contato,@PathVariable Long id ){
+		return new ResponseEntity<>(contatoService.update(contato), HttpStatus.CREATED);
+	}
+	
 	
 	@DeleteMapping("/{id}")
 	public ResponseEntity<?> delete(@PathVariable Long id) {
 		contatoService.delete(id);
 		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 	}
-	
-	
-	
-
 }
